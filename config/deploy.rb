6cc211aafc59fa3bfb31a3a0590de2a560723932
Philip Eberhardt <peberhardt@gmail.com>
2011-09-25 17:27:06 -0400

    cap deploy script for dreamhost

---
+++ config/deploy.rb
@@ -0,0 +1,44 @@
+require 'bundler/capistrano'
+
+default_run_options[:pty] = false
+ssh_options[:forward_agent] = true
+set :use_sudo, false
+set :user, "landru13"
+
+set :application, "fablicator.com"
+set :repository,  "git@github.com:rockshandy/fablicator.com.git"
+
+set :scm, :git
+set :branch, :masteri
+set :git_shallow_clonne, 1
+set :deploy_via, :remote_cache
+set :copy_compression, :bz2
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+set :rails_env, :production
+set :deploy_to, "/home/landru13/#{application}"
+
+role :web, "#{application}"                          # Your HTTP server, Apache/etc
+role :app, "#{application}"                          # This may be the same as your `Web` server
+role :db,  "#{application}", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+# restart for passenger
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
