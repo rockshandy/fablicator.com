76a95149e0a4a0e8a55c8b1dcb1d76d52ba08794
Philip Eberhardt <peberhardt@gmail.com>
2011-09-25 17:26:46 -0400

    initial commit

---
+++ .vimrc
@@ -0,0 +1,2 @@
+te_on_use_flag=1
+rvm use 1.8.7@fablicator.com
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FablicatorCom::Application.config.secret_token = '1abd0bef34f0ed04aae099a3c99e95a0c8142dbfedbbf04ff11a09641cfd82f6cd28c1bd53ea92b5a2d086e8c13ef6bbbb4e13025a5787f19e5c04bede233bcf'
