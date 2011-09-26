315415c2c0ee6a6e4822144c85ea29197de475db
Philip Eberhardt <peberhardt@gmail.com>
2011-09-25 23:11:04 -0400

    I will win yet dreamshost

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,8 @@
+Gem.clear_paths
 require 'rubygems'
-
 # Set up gems listed in the Gemfile.
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+if File.exist?(File.expand_path('../../Gemfile', __FILE__))
+  require 'bundler'
+  Bundler.setup
+end
 
-require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,14 @@
+Gem.clear_paths
+require 'rubygems'
+# Set up gems listed in the Gemfile.
+if File.exist?(File.expand_path('../../Gemfile', __FILE__))
+  require 'bundler'
+  Bundler.setup
+end
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 FablicatorCom::Application.initialize!
+
