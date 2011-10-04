23e22df81cd5ffe29151c2aa23885d5b46d08d57
Philip Eberhardt <peberhardt@gmail.com>
2011-10-03 22:44:42 -0400

    and simple tabs to handle albums by tab

--- app/assets/javascripts/welcome.gallery.js
+++ app/assets/javascripts/welcome.gallery.js
@@ -1,7 +1,16 @@
 // file for the gallery page to seetup galleria and picasa goodness
-function picasa_galleria(){
+$(function(){
+  $('#tabs a').click(function(){
+    $('.selected').removeClass('selected')
+    $(this).closest('li').addClass('selected')
+    picasa_galleria($(this).text())
+  });
+});
+
+function picasa_galleria(gallery){
+  $('.galleria-container').remove();
   var picasa = new Galleria.Picasa().setOptions({description: true});
-  picasa.useralbum('112269260036389988928','Printed', function(data) {
+  picasa.useralbum('112269260036389988928',gallery, function(data) {
       $('#galleria').galleria({
           dataSource: data,
           width: 700,
